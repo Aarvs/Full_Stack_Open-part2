85720b54d1e461d89db34f58d97f7892281b8573
arnavsharma <arnavsh2004@gmail.com>
2023-08-12 12:29:02 +0530

    use json server package for storing data and axios library for making request to the server

--- exercise-2.6.0-2.10/src/App.js
+++ exercise-2.6.0-2.10/src/App.js
@@ -1,18 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Person from "./components/person";
 import Filter from "./components/filter";
 import PersonForm from "./components/personForm";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((Response) => {
+      console.log("promise fulfilled");
+      setPersons(Response.data);
+      console.log(Response.data);
+    });
+  },[]);
+  console.log(persons.length, "person");
 
   const handleFormChange = (event) => {
     event.preventDefault();
